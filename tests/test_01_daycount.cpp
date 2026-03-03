5aeceb11d96a5786ed20b1cb1db4da333d92c23f
davidvigo0201 <david.vigo0201@gmail.com>
2026-03-03 14:03:05 +0000

    hice cambios en el test 01 de daycount

--- tests/test_01_daycount.cpp
+++ tests/test_01_daycount.cpp
@@ -2,6 +2,7 @@
 #include "quant/daycount/Actual_360.hpp"
 
 BOOST_AUTO_TEST_CASE(test_year_fractions_calculation) {
+    BOOST_TEST_MESSAGE("=== Iniciando test de fracciones de año ===");
     Quant::Time::Actual_360 act360;
     
     // Entre el 2016-04-01 y el 2016-10-03 hay exactamente 185 días
@@ -9,5 +10,9 @@ BOOST_AUTO_TEST_CASE(test_year_fractions_calculation) {
     double yf = act360("2016-04-01", "2016-10-03");
     double expected = 185.0 / 360.0;
     
+    BOOST_TEST_MESSAGE(" - yf calculado: " << yf);
+    BOOST_TEST_MESSAGE(" - expected: " << expected);
+    BOOST_TEST_MESSAGE(" - diferencia: " << (yf - expected));
+
     BOOST_CHECK_CLOSE(yf, expected, 0.0001);
 }
